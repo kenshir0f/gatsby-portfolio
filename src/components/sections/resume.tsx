cf85a1f9650110065a7aa9ff0dc131df41c8dbcb
kenshir0f <khts.wir17@gmail.com>
2021-05-30 21:31:06 +0900

    Fix resume margin

--- src/components/sections/resume.tsx
+++ src/components/sections/resume.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { colors } from '../../utils/colors'
 import { BaseProps } from '../../utils/constants'
+import { theme } from '../../utils/theme'
 import { Box } from '../box'
 import { Heading } from '../text'
 
@@ -18,10 +19,7 @@ export const Resume: React.FC<BaseProps> = ({ ...props }) => {
       </Description>
       <Department>Komerco Dept | 2017 - 2020</Department>
       <Description>デザイン基盤グループ長</Description>
-      <CompanyHistory>
-        サービスの立ち上げからUI デザイン、Webアプリケーションの開発(Next.js x
-        Firebase)
-      </CompanyHistory>
+      <CompanyHistory>Chikaku, Inc. | 2015 - 2018</CompanyHistory>
       <Department>Designer as Intern</Department>
       <Description>
         サービスの立ち上げからUI デザイン、Webアプリケーションの開発(Next.js x
@@ -33,12 +31,21 @@ export const Resume: React.FC<BaseProps> = ({ ...props }) => {
 
 const CompanyHistory = styled.h3`
   color: ${colors.black[900]};
+  font-size: ${theme.fontSizes.md};
+  margin-bottom: 20px;
+  letter-spacing: 0.02rem;
 `
 
 const Department = styled.p`
   color: ${colors.black[700]};
+  font-size: ${theme.fontSizes.xs};
+  margin-bottom: 8px;
+  letter-spacing: 0.01rem;
 `
 
 const Description = styled.p`
   color: ${colors.black[700]};
+  font-size: ${theme.fontSizes.sm};
+  margin-bottom: 24px;
+  letter-spacing: 0.01rem;
 `
--- src/utils/globalStyle.ts
+++ src/utils/globalStyle.ts
@@ -1,13 +1,13 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-html, body {
+html, body, h1, h2, h3, p, span, small, div, a {
   margin: 0;
   padding: 0;
 }
 * {
   box-sizing: border-box;
-  font-family: 'Nunito Sans', -apple-system, BlinkMacSystemFont, "SF Pro JP", "Hiragino Kaku Gothic Pro", 'Noto Sans JP', "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  font-family: 'Nunito Sans', -apple-system, BlinkMacSystemFont, "SF Pro JP", "Hiragino Kaku Gothic Pro", 'Noto Sans JP', "Segoe UI", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
 }
 `
