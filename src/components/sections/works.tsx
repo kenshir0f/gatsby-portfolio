af6d05192aa97c3bddea3881890d5dc43679e2b6
kenshir0f <khts.wir17@gmail.com>
2021-05-30 22:34:19 +0900

    Implement works

--- src/components/sections/works.tsx
+++ src/components/sections/works.tsx
@@ -1,27 +1,76 @@
 import React from 'react'
 import styled from 'styled-components'
 import { colors } from '../../utils/colors'
-import { BaseProps, COMMON, COMMON_PROPS } from '../../utils/constants'
+import { BaseProps } from '../../utils/constants'
 import { theme } from '../../utils/theme'
 import { Box, Grid } from '../box'
 import { Heading } from '../text'
 import { StaticImage } from 'gatsby-plugin-image'
 
-const komerco_jpg = '../../images/works/komerco.jpg'
-const stella_jpg = '../../images/works/stella.jpg'
-
 export const Works: React.FC<BaseProps> = ({ ...props }) => {
   return (
     <Box {...props}>
       <Heading icon="book" mb={3} text="Works" />
-      <Grid>
-        <Box>
-
-        </Box>
+      <Grid
+        gridGap="24px"
+      >
+        <Work
+          image={<StopCovidImage />}
+          title="東京都 新型コロナウイルス感染症対策サイト"
+          description="Frontend Dev / UI Design"
+        />
+        <Work
+          image={<StellaImage />}
+          title="Stella"
+          description="Frontend Dev / UI Design"
+        />
+        <Work
+          image={<KomercoImage />}
+          title="Komerco"
+          description="Frontend Dev / UI Design"
+        />
+        <Work
+          image={<MagoChImage />}
+          title="まごチャンネル"
+          description="Frontend Dev / UI Design"
+        />
       </Grid>
     </Box>
   )
 }
+
+interface Props {
+  image: JSX.Element
+  title: string
+  description: string
+}
+
+const Work: React.FC<Props> = ({ image, title, description }) => {
+  return (
+    <Box>
+      {image}
+      <Title>{title}</Title>
+      <Desc>{description}</Desc>
+    </Box>
+  )
+}
+
+const Title = styled.p`
+  color: ${colors.black[900]};
+  font-weight: 600;
+  font-size: ${theme.fontSizes.lg};
+  margin-top: 12px;
+  line-height: 125%;
+  letter-spacing: 0.02rem;
+`
+
+const Desc = styled.span`
+  color: ${colors.black[500]};
+  font-size: ${theme.fontSizes.sm};
+  margin-top: 8px;
+  line-height: 125%;
+  letter-spacing: 0.01rem;
+`
 
 // Fixme: gatsby-plugin-image では props 用いて静的画像を操作することができないため、冗長になるが4つ分の画像を用意
 // ref: https://www.gatsbyjs.com/plugins/gatsby-plugin-image/#restrictions-on-using-staticimage
@@ -30,9 +79,7 @@ const StopCovidImage: React.FC = () => {
     <StaticImage
       src="../../images/works/stop-covid.jpg"
       alt="東京都 新型コロナウイルス感染症対策サイト"
-      width={160}
-      height={160}
-      style={{ borderRadius: 4 }}
+      style={{ borderRadius: 4, objectFit: 'cover', width: '100%' }}
       placeholder="blurred"
     />
   )
@@ -43,9 +90,7 @@ const StellaImage: React.FC = () => {
     <StaticImage
       src="../../images/works/stella.jpg"
       alt="Stella"
-      width={160}
-      height={160}
-      style={{ borderRadius: 4 }}
+      style={{ borderRadius: 4, objectFit: 'cover', width: '100%' }}
       placeholder="blurred"
     />
   )
@@ -56,9 +101,7 @@ const KomercoImage: React.FC = () => {
     <StaticImage
       src="../../images/works/komerco.jpg"
       alt="Komerco"
-      width={160}
-      height={160}
-      style={{ borderRadius: 4 }}
+      style={{ borderRadius: 4, objectFit: 'cover', width: '100%' }}
       placeholder="blurred"
     />
   )
@@ -69,9 +112,7 @@ const MagoChImage: React.FC = () => {
     <StaticImage
       src="../../images/works/mago-ch.jpg"
       alt="まごチャンネル"
-      width={160}
-      height={160}
-      style={{ borderRadius: 4 }}
+      style={{ borderRadius: 4, objectFit: 'cover', width: '100%' }}
       placeholder="blurred"
     />
   )
